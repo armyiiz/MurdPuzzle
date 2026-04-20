11945bc448c38b500f668168c33457afdf2358d1
armyiiz <armkuybig2017@gmail.com>
2026-04-21 03:36:55 +0700

    Update allCases.ts

--- src/data/allCases.ts
+++ src/data/allCases.ts
@@ -1,16 +1,12 @@
+import { LevelData } from '../types/level';
 import level1 from './level1.json';
 import level2 from './level2.json';
 import level3 from './level3.json';
 import level4 from './level4.json';
-import { LevelData } from '../types/level';
 
-export type LevelsMap = {
-  [key: number]: LevelData[];
-};
-
-export const allCases: LevelsMap = {
+export const allCases: Record<number, LevelData[]> = {
   1: level1 as LevelData[],
   2: level2 as LevelData[],
   3: level3 as LevelData[],
-  4: level4 as LevelData[],
+  4: level4 as LevelData[]
 };
